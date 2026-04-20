cf57832dc90951808d1b04cc2485ba739c16daed
USB-mouse <nicsal0124@skola.goteborg.se>
2026-04-20 12:05:22 +0200

    Slut lektion 20/4
    Ändringar
    Försöker just nu lägga till if i index där man ska kunna filtera movie table med order by genom när de lades till (gjorde en ny variable i db) och alphabet etc.
    La till nytt i Main
    Höll på göra nytt i info, men det fungerade inte så bra och bestämde mig att inte ha det
    Ändrade så att nyaste visas i wishlist
    
    Known issues
    Darkmode knapp fungerar inte i filer i log mapp.

--- Slutprojekt/public_html/Slutprojekt/index.php
+++ Slutprojekt/public_html/Slutprojekt/index.php
@@ -4,24 +4,29 @@ $movieStmt = $pdo->prepare("SELECT * FROM MOVIES");
 $movieStmt -> execute();
 $movieResult = $movieStmt->fetchAll();
 
+$movieStmt = "SELECT * FROM MOVIES";
+
+/* if () {
+    $movieStmt += "ORDER BY DateAdded DESC";
+}
+*/
+
+
+
+    
+
 $userId = $_SESSION['UserId'];
 
-$watchlistStmt = $pdo->prepare("SELECT * FROM WATCHLIST JOIN Movies on Watchlist.MovieId = Movies.MovieId WHERE Watchlist.UserId = :UserId ORDER BY DateAdded ASC");
+$watchlistStmt = $pdo->prepare("SELECT * FROM WATCHLIST JOIN Movies on Watchlist.MovieId = Movies.MovieId WHERE Watchlist.UserId = :UserId ORDER BY DateAdded DESC");
 $watchlistStmt -> execute([
     'UserId' => $userId
 ]);
 $watchlistResult = $watchlistStmt->fetchAll();
 
-v
 
 $view["watchlist"] = $watchlistResult;
 
-
-
-
 $view["movies"] = $movieResult;
-
-
 
 
 $twig->display('Main.html.twig', context: $view);
