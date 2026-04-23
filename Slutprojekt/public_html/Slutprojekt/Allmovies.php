e4e211c4508fd3f80de45d7f144a17de5d2cf69e
USB-mouse <nicsal0124@skola.goteborg.se>
2026-04-23 11:30:49 +0200

    Slut lektion 04/23
    Ändringar
    Gjort nu så man kan filtera med order by
    Har försökt fixa att man kan filtiera med genre och har fixat php kod för det men håller på försöka på på ett bra sätt att implementra det
    
    Known issues
    Darkmode knapp fungerar inte i filer i log mapp.

---
+++ Slutprojekt/public_html/Slutprojekt/Allmovies.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../../Slutprojekt-app.php');
+//grunden av att hämta 
+$sql = "SELECT * FROM Movies";
+$params = [];
+//vi ska kunna se om man har tryckt på en filter 
+$sort = $_GET['sort'] ?? '';
+$genre = $_GET['genre'] ?? '';
+
+if ($genre != '') {
+    $sql .= " WHERE Genre = :genre";
+    $execute['genre'] = $genre;
+}
+
+//sorterar på vad man har tryckt med get 
+if ($sort === 'newest') {
+    $sql .= " ORDER BY DateAdded DESC";
+} elseif ($sort === 'al') {
+    $sql .= " ORDER BY Title ASC";
+}
+
+
+
+$movieStmt = $pdo->prepare($sql);
+$movieStmt -> execute($execute);
+$movieResult = $movieStmt->fetchAll();
+
+$view['movies'] = $movieResult;
+
+$twig->display('Allmovies.html.twig', context: $view);
--- Slutprojekt/public_html/Slutprojekt/index.php
+++ Slutprojekt/public_html/Slutprojekt/index.php
@@ -1,21 +1,26 @@
 <?php
 require_once('../../Slutprojekt-app.php');
-$movieStmt = $pdo->prepare("SELECT * FROM MOVIES");
+//grunden av att hämta 
+$sql = "SELECT * FROM Movies";
+//vi ska kunna se om man har tryckt på en filter 
+$sort = $_GET['sort'] ?? '';
+
+
+if ($sort === 'newest') {
+    $sql .= " ORDER BY DateAdded DESC";
+} elseif ($sort === 'al') {
+    $sql .= " ORDER BY Title ASC";
+}
+
+$movieStmt = $pdo->prepare($sql);
 $movieStmt -> execute();
 $movieResult = $movieStmt->fetchAll();
 
-$movieStmt = "SELECT * FROM MOVIES";
 
-/* if () {
-    $movieStmt += "ORDER BY DateAdded DESC";
-}
-*/
+$view['movies'] = $movieResult;
+    
 
-
-
-    h
-
-$userId = $_SESSION['UserId'];
+$userId = $_SESSION['UserId'] ?? '';
 
 $watchlistStmt = $pdo->prepare("SELECT * FROM WATCHLIST JOIN Movies on Watchlist.MovieId = Movies.MovieId WHERE Watchlist.UserId = :UserId ORDER BY DateAdded DESC");
 $watchlistStmt -> execute([
